becf65e69bf4cc1361bff47ce6b8c6e06d0e3290
Shiv Dadhania <shivdadhaniya105@gmail.com>
2024-06-25 15:45:40 +0530

    update : otpModal ui complete

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -7,20 +7,36 @@ import {
   InputAdornment,
   Checkbox,
   Divider,
+  Modal,
+  Typography,
 } from "@mui/material";
 import addSymbol from "../../assets/img/add-symbol.svg";
 import googleIcon from "../../assets/img/google.svg";
 import ScrollAnimation from "react-animate-on-scroll";
 import { Link } from "react-router-dom";
+import otpSentIcon from "../../assets/img/otp-sent.png";
+import { MuiOtpInput } from "mui-one-time-password-input";
 
 const label = { inputProps: { "aria-label": "Checkbox demo" } };
 
 class Signin extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      open: false,
+      otp: "",
+    };
+    this.handleChange = this.handleChange.bind(this);
   }
-
+  handleOpen = () => {
+    this.setState({ open: true });
+  };
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+  handleChange(newValue) {
+    this.setState({ otp: newValue });
+  }
   render() {
     return (
       <Box className="signin-container">
@@ -30,7 +46,7 @@ class Signin extends Component {
               <Box className="signin-form-details d-flex justify-content-center">
                 <Box className="d-block">
                   <h2 className="title">Register or Sign-in to My Account</h2>
-                  <Box className="add-account-symbol d-flex justify-content-center">
+                  <Box className="common-symbol d-flex justify-content-center">
                     <Box className="outer-symbol">
                       <Box className="inner-box">
                         <img src={addSymbol} alt="add-symbol" />
@@ -45,9 +61,9 @@ class Signin extends Component {
                     <img src={googleIcon} alt="google-icon" />
                   </Button>
                   <Box className="d-flex w-100 align-items-center more-option">
-                    <Divider/>
-                   <span> OR</span>
-                    <Divider/>
+                    <Divider />
+                    <span> OR</span>
+                    <Divider />
                   </Box>
                   <Box className="number-input">
                     <label className="d-block">
@@ -72,7 +88,12 @@ class Signin extends Component {
                       <Link to="">Privacy Policy.</Link>
                     </span>
                   </Box>
-                  <Button variant="contained" fullWidth className="common-btn">
+                  <Button
+                    variant="contained"
+                    fullWidth
+                    className="common-btn"
+                    onClick={this.handleOpen}
+                  >
                     Get OTP
                   </Button>
                 </Box>
@@ -80,6 +101,48 @@ class Signin extends Component {
             </Box>
           </ScrollAnimation>
         </Container>
+        <Modal
+          open={this.state.open}
+          onClose={this.handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box className="otp-modal-container">
+            <Box className="common-symbol d-flex justify-content-center">
+              <Box className="outer-symbol">
+                <Box className="inner-box">
+                  <img src={otpSentIcon} alt="add-symbol" />
+                </Box>
+              </Box>
+            </Box>
+            <Box className="text-center">
+              <h3 className="title">Enter Verification Code</h3>
+              <span className="code-send-number">
+                We’ve sent a code to <strong>+91 922011122</strong>
+              </span>
+            </Box>
+            <Divider />
+            <MuiOtpInput
+              value={this.state.otp}
+              onChange={this.handleChange}
+              className="otp-input-container"
+            />
+            <Button
+              variant="contained"
+              fullWidth
+              className="common-btn"
+              onClick={this.handleOpen}
+            >
+              Verify
+            </Button>
+            <Box className="resend-code-container text-center d-block">
+              <span className="code-issue d-block">
+                Experiencing issues receiving the code?
+              </span>
+              <Link>Resend code</Link>
+            </Box>
+          </Box>
+        </Modal>
       </Box>
     );
   }
