76a4efba8765270a2914c81095d361dd890010c6
Shiv Dadhania <shivdadhaniya105@gmail.com>
2024-06-26 11:59:52 +0530

    update : myCart page ui complete

--- src/pages/MyCart/index.js
+++ src/pages/MyCart/index.js
@@ -6,6 +6,7 @@ import realtedProdctImg1 from "../../assets/img/realted-product-1.png";
 import starIcon from "../../assets/img/star.png";
 import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import productCartImg from "../../assets/img/product-cart-img.png";
 
 class MyCart extends Component {
   constructor(props) {
@@ -44,6 +45,142 @@ class MyCart extends Component {
               </Button>
             </Box>
           </Box>
+          <Box className="cart-item-list">
+            <Grid
+              container
+              spacing={2}
+              alignItems={"center"}
+              className="cart-item-title"
+              data-aos="zoom-in-right"
+            >
+              <Grid item xs={6}>
+                Items : 3
+              </Grid>
+              <Grid item xs={3}>
+                Quantity
+              </Grid>
+              <Grid item xs={3} justifyContent={"end"} display={"flex"}>
+                Sub total
+              </Grid>
+            </Grid>
+            <Grid
+              container
+              spacing={2}
+              className="product-cart-container"
+              alignItems={"center"}
+              data-aos="zoom-in-right"
+            >
+              <Grid item xs={6}>
+                <Box className="d-flex align-items-center product-cart-list">
+                  <img src={productCartImg} alt="product-cart-img" />
+                  <Box className="d-block">
+                    <span className="d-block name">Green Apple</span>
+                    <Box className="d-flex align-items-center">
+                      <span className="discount-amount">₹ 14.99</span>
+                      <s className="amount">₹ 20.99</s>
+                    </Box>
+                  </Box>
+                </Box>
+              </Grid>
+              <Grid item xs={3}>
+                <Box className="number-input-container">
+                  <Box className="symbol">-</Box>
+                  <Box className="Number">3</Box>
+                  <Box className="symbol">+</Box>
+                </Box>
+                <Box className="d-flex align-items-ceneter btn-group">
+                  <Button>Delete</Button>
+                  <Button>Save it for later</Button>
+                </Box>
+              </Grid>
+              <Grid item xs={3} justifyContent={"end"} display={"flex"}>
+                <Box className="sub-total ">
+                  <span className="d-block final-amount">₹ 200.12</span>
+                  <span className="d-block save-amount">
+                    Saved : <strong>₹ 120.12</strong>
+                  </span>
+                </Box>
+              </Grid>
+            </Grid>
+            <Grid
+              container
+              spacing={2}
+              className="product-cart-container"
+              alignItems={"center"}
+              data-aos="zoom-in-right"
+            >
+              <Grid item xs={6}>
+                <Box className="d-flex align-items-center product-cart-list">
+                  <img src={productCartImg} alt="product-cart-img" />
+                  <Box className="d-block">
+                    <span className="d-block name">Green Apple</span>
+                    <Box className="d-flex align-items-center">
+                      <span className="discount-amount">₹ 14.99</span>
+                      <s className="amount">₹ 20.99</s>
+                    </Box>
+                  </Box>
+                </Box>
+              </Grid>
+              <Grid item xs={3}>
+                <Box className="number-input-container">
+                  <Box className="symbol">-</Box>
+                  <Box className="Number">3</Box>
+                  <Box className="symbol">+</Box>
+                </Box>
+                <Box className="d-flex align-items-ceneter btn-group">
+                  <Button>Delete</Button>
+                  <Button>Save it for later</Button>
+                </Box>
+              </Grid>
+              <Grid item xs={3} justifyContent={"end"} display={"flex"}>
+                <Box className="sub-total ">
+                  <span className="d-block final-amount">₹ 200.12</span>
+                  <span className="d-block save-amount">
+                    Saved : <strong>₹ 120.12</strong>
+                  </span>
+                </Box>
+              </Grid>
+            </Grid>
+            <Grid
+              container
+              spacing={2}
+              className="product-cart-container"
+              alignItems={"center"}
+              data-aos="zoom-in-right"
+            >
+              <Grid item xs={6}>
+                <Box className="d-flex align-items-center product-cart-list">
+                  <img src={productCartImg} alt="product-cart-img" />
+                  <Box className="d-block">
+                    <span className="d-block name">Green Apple</span>
+                    <Box className="d-flex align-items-center">
+                      <span className="discount-amount">₹ 14.99</span>
+                      <s className="amount">₹ 20.99</s>
+                    </Box>
+                  </Box>
+                </Box>
+              </Grid>
+              <Grid item xs={3}>
+                <Box className="number-input-container">
+                  <Box className="symbol">-</Box>
+                  <Box className="Number">3</Box>
+                  <Box className="symbol">+</Box>
+                </Box>
+                <Box className="d-flex align-items-ceneter btn-group">
+                  <Button>Delete</Button>
+                  <Button>Save it for later</Button>
+                </Box>
+              </Grid>
+              <Grid item xs={3} justifyContent={"end"} display={"flex"}>
+                <Box className="sub-total ">
+                  <span className="d-block final-amount">₹ 200.12</span>
+                  <span className="d-block save-amount">
+                    Saved : <strong>₹ 120.12</strong>
+                  </span>
+                </Box>
+              </Grid>
+            </Grid>
+          </Box>
         </Container>
         <Box className="Related-products-container">
           <Container>
@@ -53,7 +190,7 @@ class MyCart extends Component {
                 <span className="d-block">Show All</span>
               </Link>
             </Box>
-            <Grid container spacing={2}>
+            <Grid container spacing={2} data-aos="zoom-in">
               <Grid item xs={4}>
                 <Box className="product-details d-flex align-items-center ">
                   <img src={realtedProdctImg1} alt="" />
