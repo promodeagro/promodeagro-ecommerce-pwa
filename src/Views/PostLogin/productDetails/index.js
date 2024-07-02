ba3ec046e06fa1bc3115f50cbc72dd825a9708b8
Satishpal93 <satishweb93@gmail.com>
2024-07-02 15:56:44 +0530

    productDetails page UI

--- src/Views/PostLogin/productDetails/index.js
+++ src/Views/PostLogin/productDetails/index.js
@@ -1,5 +1,13 @@
 import React, { Component } from "react";
-import { Box, Button, Container, Grid } from "@mui/material";
+import {
+  Box,
+  Button,
+  Container,
+  Grid,
+  Rating,
+  TextField,
+  InputAdornment,
+} from "@mui/material";
 import RecentlyViewedItems from "./recentlyViewedItems";
 import FacebookOutlinedIcon from "@mui/icons-material/FacebookOutlined";
 import TwitterIcon from "@mui/icons-material/Twitter";
@@ -7,10 +15,32 @@ import InstagramIcon from "@mui/icons-material/Instagram";
 import AttachmentIcon from "@mui/icons-material/Attachment";
 import StarIcon from "@mui/icons-material/Star";
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import CheckCircleOutlinedIcon from "@mui/icons-material/CheckCircleOutlined";
+import ExpandCircleDownOutlinedIcon from "@mui/icons-material/ExpandCircleDownOutlined";
+import TurnedInNotOutlinedIcon from "@mui/icons-material/TurnedInNotOutlined";
 import productBigImage from "../../../assets/img/product-big-image.png";
 import productThumbnailImage from "../../../assets/img/product-big-image.png";
 import mdiRupee from "../../../assets/img/mdi-rupee.png";
 import rupeeIcon from "../../../assets/img/rupee.png";
+import deliveryTruck from "../../../assets/img/delivery-truck.png";
+import timeIcon from "../../../assets/img/time-icon.png";
+import returnIcon from "../../../assets/img/return-icon.png";
+import organicIcon from "../../../assets/img/organic-icon.png";
+import searchIcon from "../../../assets/img/search-icon.png";
+import reviewImg from "../../../assets/img/review-img.png";
+
+const labels = {
+  0.5: "0.5 out of 5",
+  1: "1 out of 5",
+  1.5: "1.5 out of 5",
+  2: "2 out of 5",
+  2.5: "2.5 out of 5",
+  3: "3 out of 5",
+  3.5: "3.5 out of 5",
+  4: "4 out of 5",
+  4.5: "4.5 out of 5",
+  5: "5 out of 5",
+};
 
 class ProductDetails extends Component {
   constructor(props) {
@@ -19,6 +49,7 @@ class ProductDetails extends Component {
   }
 
   render() {
+    const value = 4.5;
     return (
       <Box className="main-container">
         <Container>
@@ -49,9 +80,12 @@ class ProductDetails extends Component {
           </Box>
           <Box className="details-container">
             <Grid container spacing={2}>
-              <Grid item xs={6} sm={6} md={6}>
+              <Grid item xs={6} sm={6} md={5}>
                 <Box className="product-images">
                   <Box className="big-image">
+                    <Box className="icon">
+                      <TurnedInNotOutlinedIcon />
+                    </Box>
                     <img src={productBigImage} alt="" />
                   </Box>
                   <Box className="thumbnail-images">
@@ -80,7 +114,7 @@ class ProductDetails extends Component {
                   </Box>
                 </Box>
               </Grid>
-              <Grid item xs={6} sm={6} md={6}>
+              <Grid item xs={6} sm={6} md={7}>
                 <Box className="product-info">
                   <Box className="product-name">Green Apple</Box>
                   <Box className="product-review">
@@ -122,19 +156,330 @@ class ProductDetails extends Component {
                     <Box
                       display={"flex"}
                       alignItems={"flex-start"}
-                      justifyContent={"flex-start"}
+                      justifyContent={"space-between"}
                       width={"100%"}
+                      flexWrap={"wrap"}
                     >
-                      <Box className="pack-box">
-                        <Grid container spacing={2}>
-                          <Grid item xs={6} sm={6} md={6}>
-                            <Box className="left-contents"></Box>
+                      <Box className="pack-box active">
+                        <Box className="check">
+                          <CheckCircleOutlinedIcon />
+                        </Box>
+                        <Grid container spacing={0}>
+                          <Grid item xs={6} sm={6} md={5}>
+                            <Box className="left-contents">
+                              <strong>2x4 pcs</strong>
+                              <span>Multipack</span>
+                            </Box>
                           </Grid>
-                          <Grid item xs={6} sm={6} md={6}>
-                            <Box className="left-contents"></Box>
+                          <Grid item xs={6} sm={6} md={7}>
+                            <Box className="right-contents">
+                              <Box className="product-price">
+                                <Box className="price">
+                                  <img src={rupeeIcon} alt="" /> 200.12
+                                  <span>
+                                    <img src={mdiRupee} alt="" />
+                                    30.12 / pc
+                                  </span>
+                                </Box>
+                              </Box>
+                              <Box className="product-save">
+                                <p>
+                                  <img src={mdiRupee} alt="" /> 320.99
+                                </p>
+                                <span>20% OFF</span>
+                              </Box>
+                            </Box>
                           </Grid>
                         </Grid>
                       </Box>
+                      <Box className="pack-box">
+                        <Grid container spacing={0}>
+                          <Grid item xs={6} sm={6} md={5}>
+                            <Box className="left-contents">
+                              <strong>4 pcs</strong>
+                              <span>Approx. 500-600gm</span>
+                            </Box>
+                          </Grid>
+                          <Grid item xs={6} sm={6} md={7}>
+                            <Box className="right-contents">
+                              <Box className="product-price">
+                                <Box className="price">
+                                  <img src={rupeeIcon} alt="" /> 200.12
+                                  <span>
+                                    <img src={mdiRupee} alt="" />
+                                    30.12 / pc
+                                  </span>
+                                </Box>
+                              </Box>
+                              <Box className="product-save">
+                                <p>
+                                  <img src={mdiRupee} alt="" /> 320.99
+                                </p>
+                                <span>20% OFF</span>
+                              </Box>
+                            </Box>
+                          </Grid>
+                        </Grid>
+                      </Box>
+                      <Box className="pack-box">
+                        <Box className="check">
+                          <CheckCircleOutlinedIcon />
+                        </Box>
+                        <Grid container spacing={0}>
+                          <Grid item xs={6} sm={6} md={5}>
+                            <Box className="left-contents">
+                              <strong>4 pcs</strong>
+                              <span>Multipack</span>
+                            </Box>
+                          </Grid>
+                          <Grid item xs={6} sm={6} md={7}>
+                            <Box className="right-contents">
+                              <Box className="product-price">
+                                <Box className="price">
+                                  <img src={rupeeIcon} alt="" /> 200.12
+                                  <span>
+                                    <img src={mdiRupee} alt="" />
+                                    30.12 / pc
+                                  </span>
+                                </Box>
+                              </Box>
+                              <Box className="product-save">
+                                <p>
+                                  <img src={mdiRupee} alt="" /> 320.99
+                                </p>
+                                <span>20% OFF</span>
+                              </Box>
+                            </Box>
+                          </Grid>
+                        </Grid>
+                      </Box>
+                      <Box className="pack-box">
+                        <Grid container spacing={0}>
+                          <Grid item xs={6} sm={6} md={5}>
+                            <Box className="left-contents">
+                              <strong>4 pcs</strong>
+                              <span>Approx. 500-600gm</span>
+                            </Box>
+                          </Grid>
+                          <Grid item xs={6} sm={6} md={7}>
+                            <Box className="right-contents">
+                              <Box className="product-price">
+                                <Box className="price">
+                                  <img src={rupeeIcon} alt="" /> 200.12
+                                  <span>
+                                    <img src={mdiRupee} alt="" />
+                                    30.12 / pc
+                                  </span>
+                                </Box>
+                              </Box>
+                              <Box className="product-save">
+                                <p>
+                                  <img src={mdiRupee} alt="" /> 320.99
+                                </p>
+                                <span>20% OFF</span>
+                              </Box>
+                            </Box>
+                          </Grid>
+                        </Grid>
+                      </Box>
+                    </Box>
+                    <Box className="combos-btn">
+                      <Button>
+                        <p>+2</p> More Combos
+                      </Button>
+                    </Box>
+                  </Box>
+                </Box>
+              </Grid>
+            </Grid>
+          </Box>
+          <Box
+            display={"flex"}
+            justifyContent={"center"}
+            width={"100%"}
+            className="choose-container"
+          >
+            <Box className="heading">Why Choose us ?</Box>
+            <Grid container spacing={5} justifyContent={"center"}>
+              <Grid item xs={6} sm={6} md={3}>
+                <Box className="choose-box">
+                  <Box className="box">
+                    <img src={deliveryTruck} alt="" />
+                  </Box>
+                  <span>Free Shipping</span>
+                  <p>Delivery at your door step</p>
+                </Box>
+              </Grid>
+              <Grid item xs={6} sm={6} md={3}>
+                <Box className="choose-box">
+                  <Box className="box">
+                    <img src={timeIcon} alt="" />
+                  </Box>
+                  <span>On Time</span>
+                  <p>Guarantee</p>
+                </Box>
+              </Grid>
+              <Grid item xs={6} sm={6} md={3}>
+                <Box className="choose-box">
+                  <Box className="box">
+                    <img src={returnIcon} alt="" />
+                  </Box>
+                  <span>Easy Return</span>
+                  <p>No Questions Asked</p>
+                </Box>
+              </Grid>
+              <Grid item xs={6} sm={6} md={3}>
+                <Box className="choose-box">
+                  <Box className="box">
+                    <img src={organicIcon} alt="" />
+                  </Box>
+                  <span>100% Organic</span>
+                  <p>You can Trust</p>
+                </Box>
+              </Grid>
+            </Grid>
+          </Box>
+          <Box className="product-contents">
+            <h3>About the Product</h3>
+            <p>
+              Granny Smith apples are light green in colour. They are popularly
+              used in manyapple dishes, such asapple pie, applecobbler,apple
+              crisp, andapple cake. They are also commonly eaten raw astable
+              apples. Granny Smith are high in antioxidant activity, and they
+              boast the highest concentration of phenols amongst the apple
+              breeds, efficient source of antioxidants, particularly the
+              flavonoids cyanidin and epicatechin, especially if eaten with the
+              skin intact. Granny Smiths are also naturally low in calories and
+              high in dietary fiber and potassium, making them commonly
+              recommended as a component of healthy and weight-loss diets. Click
+              here for delicious fruit recipes
+            </p>
+            <Box className="other-info">
+              <h4>
+                Other Product Info <ExpandCircleDownOutlinedIcon />
+              </h4>
+              <ul>
+                <li>EAN Code: 1203789</li>
+                <li>Country of origin: India</li>
+              </ul>
+            </Box>
+          </Box>
+          <Box className="reviews-container">
+            <Grid container spacing={2}>
+              <Grid item xs={6} sm={6} md={4}>
+                <Box className="heading">Customer Reviews</Box>
+                <Box className="rating">
+                  <Rating
+                    name="text-feedback"
+                    value={value}
+                    readOnly
+                    precision={0.5}
+                    emptyIcon={
+                      <StarIcon style={{ opacity: 0 }} fontSize="inherit" />
+                    }
+                  />
+                  <Box sx={{ ml: 1 }}>{labels[value]}</Box>
+                </Box>
+                <Box className="star-lines">
+                  <Box className="line">
+                    <span>5 Star</span>
+                    <Box className="percent-line">
+                      <Box className="percent" style={{ width: "67%" }}></Box>
+                    </Box>
+                    <p>67%</p>
+                  </Box>
+                  <Box className="line">
+                    <span>4 Star</span>
+                    <Box className="percent-line">
+                      <Box className="percent" style={{ width: "13%" }}></Box>
+                    </Box>
+                    <p>13%</p>
+                  </Box>
+                  <Box className="line">
+                    <span>3 Star</span>
+                    <Box className="percent-line">
+                      <Box className="percent" style={{ width: "10%" }}></Box>
+                    </Box>
+                    <p>10%</p>
+                  </Box>
+                  <Box className="line">
+                    <span>2 Star</span>
+                    <Box className="percent-line">
+                      <Box className="percent" style={{ width: "6%" }}></Box>
+                    </Box>
+                    <p>6%</p>
+                  </Box>
+                  <Box className="line">
+                    <span>1 Star</span>
+                    <Box className="percent-line">
+                      <Box className="percent" style={{ width: "4%" }}></Box>
+                    </Box>
+                    <p>4%</p>
+                  </Box>
+                </Box>
+                <Box className="write-review">
+                  <strong>Review this product</strong>
+                  <p>Share your thoughts with other customers</p>
+                  <Button>Write a product Review</Button>
+                </Box>
+              </Grid>
+              <Grid item xs={6} sm={6} md={8}>
+                <Box className="reviews">
+                  <Box className="search-box">
+                    <TextField
+                      id="outlined-search"
+                      className="search"
+                      variant="outlined"
+                      InputProps={{
+                        startAdornment: (
+                          <InputAdornment position="start">
+                            <img src={searchIcon} alt="" />
+                          </InputAdornment>
+                        ),
+                      }}
+                      defaultValue="Search in reviews"
+                    />
+                  </Box>
+                  <Box className="review-boxes">
+                    <Box className="review-box">
+                      <Grid container spacing={2}>
+                        <Grid item xs={6} sm={6} md={1}>
+                          <Box className="image">
+                            <img src={reviewImg} alt="" />
+                          </Box>
+                        </Grid>
+                        <Grid item xs={6} sm={6} md={11}>
+                          <Box className="contents">
+                            <Rating defaultValue={5} readOnly className="rating" />
+                            <p>
+                              "We love Landingfolio! Our designers were using it
+                              for their projects, so we already knew what kind
+                              of design they want."
+                            </p>
+                            <strong>Devon Lane</strong>
+                          </Box>
+                        </Grid>
+                      </Grid>
+                    </Box>
+                    <Box className="review-box">
+                      <Grid container spacing={2}>
+                        <Grid item xs={6} sm={6} md={1}>
+                          <Box className="image">
+                            <img src={reviewImg} alt="" />
+                          </Box>
+                        </Grid>
+                        <Grid item xs={6} sm={6} md={11}>
+                          <Box className="contents">
+                            <Rating defaultValue={5} readOnly className="rating" />
+                            <p>
+                              "We love Landingfolio! Our designers were using it
+                              for their projects, so we already knew what kind
+                              of design they want."
+                            </p>
+                            <strong>Devon Lane</strong>
+                          </Box>
+                        </Grid>
+                      </Grid>
                     </Box>
                   </Box>
                 </Box>
