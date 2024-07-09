b32570267287cb7f87f754be26ccee1607a8e93a
Aman jainwal <jainwalaman@gmail.com>
2024-07-09 15:05:43 +0530

    my cart loader issue fixed

--- src/Views/PostLogin/MyCart/index.js
+++ src/Views/PostLogin/MyCart/index.js
@@ -30,6 +30,7 @@ class MyCart extends Component {
     this.state = {
       cartList: [],
       dataId: "",
+      isUpdateIncrease: null
     };
   }
 
@@ -72,12 +73,17 @@ class MyCart extends Component {
       });
     }
   }
-
   handleQuantityChange(id, increment, productQuantity) {
     const items = loginDetails();
+    if (increment < 0 && productQuantity != 0) {
+      this.setState({ isUpdateIncrease: false });
+    } else if (productQuantity != 0) {
+      this.setState({ isUpdateIncrease: true });
+    }
     this.setState({
       dataId: id,
     });
+
     productQuantity = productQuantity + increment;
     if (productQuantity != 0) {
       this.props.updateItemToCart({
@@ -94,183 +100,189 @@ class MyCart extends Component {
   }
 
   render() {
-    const { dataId } = this.state;
 
+    const { dataId, isUpdateIncrease } = this.state;
     return (
       <>
-        {this.props.cartItems.status === status.IN_PROGRESS ? (
-          Loader.commonLoader()
-        ) : (
-          <Box className="mycart-container">
-            <Container>
-              <Box className="sub-total-container d-flex justify-content-between">
-                <Box className="left-part">
-                  <h1 className="d-block">My Cart</h1>
-                  <p className="d-block sub-heading">
-                    This is your cart based on your item you want to buy..
-                  </p>
-                </Box>
-                <Box className="right-part">
-                  <Box className="sub-total d-flex align-items-center flex-wrap">
-                    <strong className="title">Subtotal </strong>
-                    <span className="item-count">(3 Items) :</span>
-                    <strong className="number">₹ 200.12</strong>
-                  </Box>
-                  <Box className="saving-amount">
-                    <strong className="title">Savings : </strong>
-                    <strong className="number">₹ 120.12</strong>
-                  </Box>
-                  <Link to={"/myCart/address"} className="checkout-btn">
-                    <Button
-                      variant="contained"
-                      fullWidth
-                      className="common-btn"
-                      endIcon={<NavigateNextIcon />}
-                    >
-                      Checkout
-                    </Button>
-                  </Link>
-                </Box>
+        <Box className="mycart-container">
+          <Container>
+            <Box className="sub-total-container d-flex justify-content-between">
+              <Box className="left-part">
+                <h1 className="d-block">My Cart</h1>
+                <p className="d-block sub-heading">
+                  This is your cart based on your item you want to buy..
+                </p>
               </Box>
-              <Box className="cart-item-list">
-                {this.state.cartList.length > 0 ? (
-                  <>
-                    <Grid
-                      container
-                      spacing={0}
-                      alignItems={"center"}
-                      className="cart-item-title"
-                      data-aos="zoom-in-right"
-                    >
-                      <Grid item xs={6} md={6} lg={6} sm={6}>
-                        Items : 3
-                      </Grid>
-                      <Grid item xs={3} md={3} lg={3} sm={3}>
-                        Quantity
-                      </Grid>
-                      <Grid
-                        item
-                        xs={3}
-                        md={3}
-                        lg={3}
-                        sm={3}
-                        justifyContent={"end"}
-                        display={"flex"}
-                      >
-                        Sub total
-                      </Grid>
+              <Box className="right-part">
+                <Box className="sub-total d-flex align-items-center flex-wrap">
+                  <strong className="title">Subtotal </strong>
+                  <span className="item-count">(3 Items) :</span>
+                  <strong className="number">₹ 200.12</strong>
+                </Box>
+                <Box className="saving-amount">
+                  <strong className="title">Savings : </strong>
+                  <strong className="number">₹ 120.12</strong>
+                </Box>
+                <Link to={"/myCart/address"} className="checkout-btn">
+                  <Button
+                    variant="contained"
+                    fullWidth
+                    className="common-btn"
+                    endIcon={<NavigateNextIcon />}
+                  >
+                    Checkout
+                  </Button>
+                </Link>
+              </Box>
+            </Box>
+            <Box className="cart-item-list">
+              {this.state.cartList.length > 0 ? (
+                <>
+                  <Grid
+                    container
+                    spacing={0}
+                    alignItems={"center"}
+                    className="cart-item-title"
+                    data-aos="zoom-in-right"
+                  >
+                    <Grid item xs={6} md={6} lg={6} sm={6}>
+                      Items : 3
                     </Grid>
-                    {this.state.cartList.map((item) => {
-                      return (
-                        <Grid
-                          container
-                          spacing={0}
-                          className="product-cart-container"
-                          alignItems={"center"}
-                          data-aos="zoom-in-right"
-                        >
-                          {/* <Box className="bookmark-btn">
+                    <Grid item xs={3} md={3} lg={3} sm={3}>
+                      Quantity
+                    </Grid>
+                    <Grid
+                      item
+                      xs={3}
+                      md={3}
+                      lg={3}
+                      sm={3}
+                      justifyContent={"end"}
+                      display={"flex"}
+                    >
+                      Sub total
+                    </Grid>
+                  </Grid>
+                  {this.state.cartList.map((item) => {
+                    return (
+                      <Grid
+                        container
+                        spacing={0}
+                        className="product-cart-container"
+                        alignItems={"center"}
+                        data-aos="zoom-in-right"
+                      >
+                        {/* <Box className="bookmark-btn">
                             <IconButton aria-label="bookmark">
                               <BookmarkBorderIcon />
                             </IconButton>
                           </Box> */}
-                          <Grid item xs={12} sm={12} md={6} lg={6}>
-                            <Box className="d-flex align-items-center product-cart-list">
-                              <Box className="product-cart-img">
-                                <img
-                                  src={productCartImg}
-                                  alt="product-cart-img"
-                                />
-                              </Box>
-                              <Box className="d-block">
-                                <span className="d-block name">
-                                  Green Apple
+                        <Grid item xs={12} sm={12} md={6} lg={6}>
+                          <Box className="d-flex align-items-center product-cart-list">
+                            <Box className="product-cart-img">
+                              <img
+                                src={productCartImg}
+                                alt="product-cart-img"
+                              />
+                            </Box>
+                            <Box className="d-block">
+                              <span className="d-block name">
+                                Green Apple
+                              </span>
+                              <Box className="d-flex align-items-center">
+                                <span className="discount-amount">
+                                  ₹ {item?.Price}
                                 </span>
-                                <Box className="d-flex align-items-center">
-                                  <span className="discount-amount">
-                                    ₹ {item?.Price}
-                                  </span>
-                                  <s className="amount">₹ {item?.Mrp} </s>
-                                </Box>
+                                <s className="amount">₹ {item?.Mrp} </s>
                               </Box>
                             </Box>
-                          </Grid>
-                          <Grid item xs={12} sm={6} md={3} lg={3}>
-                            <Box className="number-input-container">
-                              <Box
-                                className="symbol"
-                                onClick={() => {
-                                  let d = item.Quantity;
-                                  this.handleQuantityChange(
-                                    item.ProductId,
-                                    -1,
-                                    Number(d)
-                                  );
-                                }}
-                              >
-                                -
-                              </Box>
-
-                              <Box className="Number">{item?.Quantity}</Box>
-                              <Box
-                                className="symbol"
-                                onClick={() => {
-                                  let d = item.Quantity;
-                                  this.handleQuantityChange(
-                                    item.ProductId,
-                                    1,
-                                    Number(d)
-                                  );
-                                }}
-                              >
-                                {this.props.updateItems.status ===
-                                  status.IN_PROGRESS && item.id === dataId ? (
-                                  <CircularProgress className="common-loader plus-icon" />
-                                ) : (
-                                  "+"
-                                )}
-                              </Box>
-                            </Box>
-                            <Box className="d-flex align-items-ceneter btn-group">
-                              <Button
-                                onClick={() => {
-                                  console.log("item", item);
-                                  const items = loginDetails();
-                                  this.props.deleteItemToCart({
-                                    userId: items.userId,
-                                    productId: item.ProductId,
-                                  });
-                                }}
-                              >
-                                Delete
-                              </Button>
-                              <Button>Save it for later</Button>
-                            </Box>
-                          </Grid>
-                          <Grid item xs={12} sm={6} md={3} lg={3}>
-                            <Box
-                              className="sub-total"
-                            >
-                              <span className="d-block final-amount">
-                                ₹ {item?.Subtotal}
-                              </span>
-                              <span className="d-block save-amount">
-                                Saved : <strong>₹ {item.Savings}</strong>
-                              </span>
-                            </Box>
-                          </Grid>
+                          </Box>
                         </Grid>
-                      );
-                    })}
-                  </>
-                ) : (
-                  <></>
-                )}
-              </Box>
-            </Container>
-            <RelatedViewedItems />
-          </Box>
-        )}
+                        <Grid item xs={12} sm={6} md={3} lg={3}>
+                          <Box className="number-input-container">
+
+
+
+
+                            <Box
+                              className="symbol"
+                              onClick={() => {
+                                let d = item.Quantity;
+                                this.handleQuantityChange(
+                                  item.ProductId,
+                                  -1,
+                                  Number(d)
+                                );
+                              }}
+                            >
+                              {(this.props.deleteItems.status === status.IN_PROGRESS && item.ProductId === dataId && !isUpdateIncrease) ||
+                                (this.props.updateItems.status === status.IN_PROGRESS && item.ProductId === dataId && !isUpdateIncrease) ? (
+                                <CircularProgress className="common-loader plus-icon" size={24} />
+                              ) : (
+                                "-"
+                              )}
+                            </Box>
+
+
+                            <Box className="Number">{item?.Quantity}</Box>
+                            <Box
+                              className="symbol"
+                              onClick={() => {
+                                let d = item.Quantity;
+                                this.handleQuantityChange(
+                                  item.ProductId,
+                                  1,
+                                  Number(d)
+                                );
+                              }}
+                            >
+                              {this.props.updateItems.status === status.IN_PROGRESS && item.ProductId === dataId && isUpdateIncrease ? (
+                                <CircularProgress className="common-loader plus-icon" size={24} />
+                              ) : (
+                                "+"
+                              )}
+                            </Box>
+
+                          </Box>
+                          <Box className="d-flex align-items-ceneter btn-group">
+                            <Button
+                              onClick={() => {
+                                console.log("item", item);
+                                const items = loginDetails();
+                                this.props.deleteItemToCart({
+                                  userId: items.userId,
+                                  productId: item.ProductId,
+                                });
+                              }}
+                            >
+                              Delete
+                            </Button>
+                            <Button>Save it for later</Button>
+                          </Box>
+                        </Grid>
+                        <Grid item xs={12} sm={6} md={3} lg={3}>
+                          <Box
+                            className="sub-total"
+                          >
+                            <span className="d-block final-amount">
+                              ₹ {item?.Subtotal}
+                            </span>
+                            <span className="d-block save-amount">
+                              Saved : <strong>₹ {item.Savings}</strong>
+                            </span>
+                          </Box>
+                        </Grid>
+                      </Grid>
+                    );
+                  })}
+                </>
+              ) : (
+                <></>
+              )}
+            </Box>
+          </Container>
+          <RelatedViewedItems />
+        </Box>
       </>
     );
   }
