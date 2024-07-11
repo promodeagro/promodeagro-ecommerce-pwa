2eae1eaa4dff82a768885db5601e682601ab4f66
Aman jainwal <jainwalaman@gmail.com>
2024-07-11 11:34:02 +0530

    recent view functionality done in category page

--- src/Views/PostLogin/category/recentlyViewedItems/index.js
+++ src/Views/PostLogin/category/recentlyViewedItems/index.js
@@ -9,69 +9,53 @@ import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 class RecentlyViewedItems extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      recentList: [],
+      showAllItems: false  // State to track if all items should be shown
+    };
   }
 
+  componentDidMount() {
+    let data = localStorage.getItem("recentviewitems");
+    if (data) {
+      this.setState({
+        recentList: JSON.parse(data)
+      });
+    }
+  }
+
+  // Function to toggle showing all items
+  handleShowAll = () => {
+    this.setState({
+      showAllItems: true
+    });
+  };
+
   render() {
+    const { recentList, showAllItems } = this.state;
+
     return (
       <Box className="recently-container">
         <Container>
           <Box className="heading">
             <h2>Recently Viewed Items</h2>
-            <Button>Show All</Button>
+            {/* <Button onClick={this.handleShowAll}>Show All</Button> */}
           </Box>
-          <Box className="recently-products">
+          {recentList && recentList.length > 0 ? (
+            // Render either all items or first 4 items based on showAllItems state
+            recentList.slice(0, 3).map(item => (
+          <Box key={item.id} className="recently-products">
             <Box className="product-box">
               <Grid container spacing={2} alignItems={'center'}>
                 <Grid item xs={6} sm={6} md={4}>
-                  <Box className="image"><img src={productCartImg} alt="" /></Box>
+                  <Box className="image"><img src={item?.image} alt="" /></Box>
                 </Grid>
                 <Grid item xs={6} sm={6} md={8}>
                   <Box className="contents">
-                    <Box className="name"><a href="#">Green Apple</a></Box>
-                    <Box className="price"><img src={priceIcon} alt="" /> 14.99 <span>20.99</span></Box>
+                    <Box className="name"><a href="#">{item?.name}</a></Box>
+                    <Box className="price"><img src={priceIcon} alt="" /> {item?.price}<span>{item?.mrp}</span></Box>
                     <Box display={'flex'} width={'100%'} justifyContent={'space-between'} alignItems={'center'}>
-                      <Box className="ratting"><StarIcon /> 4.5</Box>
-                      <Box className="buttons">
-                        <Button><TurnedInNotOutlinedIcon /></Button>
-                        <Button><ShoppingCartOutlinedIcon /></Button>
-                      </Box>
-                    </Box>
-                  </Box>
-                </Grid>
-              </Grid>
-            </Box>
-            <Box className="product-box">
-              <Grid container spacing={2} alignItems={'center'}>
-                <Grid item xs={6} sm={6} md={4}>
-                  <Box className="image"><img src={productCartImg} alt="" /></Box>
-                </Grid>
-                <Grid item xs={6} sm={6} md={8}>
-                  <Box className="contents">
-                    <Box className="name"><a href="#">Green Apple</a></Box>
-                    <Box className="price"><img src={priceIcon} alt="" /> 14.99 <span>20.99</span></Box>
-                    <Box display={'flex'} width={'100%'} justifyContent={'space-between'} alignItems={'center'}>
-                      <Box className="ratting"><StarIcon /> 4.5</Box>
-                      <Box className="buttons">
-                        <Button><TurnedInNotOutlinedIcon /></Button>
-                        <Button><ShoppingCartOutlinedIcon /></Button>
-                      </Box>
-                    </Box>
-                  </Box>
-                </Grid>
-              </Grid>
-            </Box>
-            <Box className="product-box">
-              <Grid container spacing={2} alignItems={'center'}>
-                <Grid item xs={6} sm={6} md={4}>
-                  <Box className="image"><img src={productCartImg} alt="" /></Box>
-                </Grid>
-                <Grid item xs={6} sm={6} md={8}>
-                  <Box className="contents">
-                    <Box className="name"><a href="#">Green Apple</a></Box>
-                    <Box className="price"><img src={priceIcon} alt="" /> 14.99 <span>20.99</span></Box>
-                    <Box display={'flex'} width={'100%'} justifyContent={'space-between'} alignItems={'center'}>
-                      <Box className="ratting"><StarIcon /> 4.5</Box>
+                      <Box className="rating"><StarIcon /> {item?.ratings}</Box>
                       <Box className="buttons">
                         <Button><TurnedInNotOutlinedIcon /></Button>
                         <Button><ShoppingCartOutlinedIcon /></Button>
@@ -82,6 +66,11 @@ class RecentlyViewedItems extends Component {
               </Grid>
             </Box>
           </Box>
+          ))
+          ) : (
+          <Box>No recently viewed items.</Box>
+          )}
+
         </Container>
       </Box>
     );
