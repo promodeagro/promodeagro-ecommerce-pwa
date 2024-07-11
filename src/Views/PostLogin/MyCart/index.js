749cbc4ecdaee67b8498cfc8ec61505b6c7b6f09
Aman jainwal <jainwalaman@gmail.com>
2024-07-11 15:00:17 +0530

    there is no data text added and checkout button hide

--- src/Views/PostLogin/MyCart/index.js
+++ src/Views/PostLogin/MyCart/index.js
@@ -107,39 +107,45 @@ class MyCart extends Component {
         <Box className="mycart-container">
           <Container>
             <Box className="sub-total-container d-flex justify-content-between">
-              <Box className="left-part">
-                <h1 className="d-block">My Cart</h1>
-                <p className="d-block sub-heading">
-                  This is your cart based on your item you want to buy..
-                </p>
-              </Box>
-              <Box className="right-part">
-                <Box className="sub-total d-flex align-items-center flex-wrap">
-                  <strong className="title">Subtotal </strong>
-                  <span className="item-count">
-                    ({this.state?.cartList?.length} Items) :
-                  </span>
-                  <strong className="number">
-                    ₹ {this.props?.cartItems?.data?.subTotal}
-                  </strong>
-                </Box>
-                <Box className="saving-amount">
-                  <strong className="title">Savings : </strong>
-                  <strong className="number">
-                    ₹ {this.props?.cartItems?.data?.savings}
-                  </strong>
-                </Box>
-                <Link to={"/myCart/address"} className="checkout-btn">
-                  <Button
-                    variant="contained"
-                    fullWidth
-                    className="common-btn"
-                    endIcon={<NavigateNextIcon />}
-                  >
-                    Checkout
-                  </Button>
-                </Link>
-              </Box>
+              {this.state.cartList.length > 0 ?
+                <>
+                  <Box className="left-part">
+                    <h1 className="d-block">My Cart</h1>
+                    <p className="d-block sub-heading">
+                      This is your cart based on your item you want to buy..
+                    </p>
+                  </Box>
+                  <Box className="right-part">
+                    <Box className="sub-total d-flex align-items-center flex-wrap">
+                      <strong className="title">Subtotal </strong>
+                      <span className="item-count">
+                        ({this.state?.cartList?.length} Items) :
+                      </span>
+                      <strong className="number">
+                        ₹ {this.props?.cartItems?.data?.subTotal}
+                      </strong>
+                    </Box>
+                    <Box className="saving-amount">
+                      <strong className="title">Savings : </strong>
+                      <strong className="number">
+                        ₹ {this.props?.cartItems?.data?.savings}
+                      </strong>
+                    </Box>
+                    <Link to={"/myCart/address"} className="checkout-btn">
+                      <Button
+                        variant="contained"
+                        fullWidth
+                        className="common-btn"
+                        endIcon={<NavigateNextIcon />}
+                      >
+                        Checkout
+                      </Button>
+                    </Link>
+                  </Box>
+                </>
+
+                : <></>}
+
             </Box>
             {this.props.cartItems.status === status.IN_PROGRESS ? (
               Loader.commonLoader()
@@ -293,7 +299,7 @@ class MyCart extends Component {
                     })}
                   </>
                 ) : (
-                  <></>
+                  <Box> There is no data </Box>
                 )}
               </Box>
             )}
