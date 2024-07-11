2d7554548a4e5bf3f7d7ddf25ae5fea6725f276d
Aman jainwal <jainwalaman@gmail.com>
2024-07-11 11:29:47 +0530

    show all option hide (recent view item)

--- src/Views/PostLogin/productDetails/recentlyViewedItems/index.js
+++ src/Views/PostLogin/productDetails/recentlyViewedItems/index.js
@@ -10,61 +10,65 @@ class RecentlyViewedItems extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      recentList: []
-
+      recentList: [],
+      showAllItems: false  // State to track if all items should be shown
     };
   }
 
   componentDidMount() {
     let data = localStorage.getItem("recentviewitems");
     if (data) {
-      this.setState(
-        {
-          recentList: JSON.parse(data)
-        })
-
+      this.setState({
+        recentList: JSON.parse(data)
+      });
     }
   }
 
+  // Function to toggle showing all items
+  handleShowAll = () => {
+    this.setState({
+      showAllItems: true
+    });
+  };
+
   render() {
-
-    const { recentList } = this.state
-
+    const { recentList, showAllItems } = this.state;
 
     return (
       <Box className="recently-container">
         <Container>
           <Box className="heading">
             <h2>Recently Viewed Items</h2>
-            <Button>Show All</Button>
+            {/* <Button onClick={this.handleShowAll}>Show All</Button> */}
           </Box>
           {recentList && recentList.length > 0 ? (
-            recentList.slice(0, 4).map(item => (
-              <Box key={item.id} className="recently-products">
-                <Box className="product-box">
-                  <Grid container spacing={2} alignItems={'center'}>
-                    <Grid item xs={6} sm={6} md={4}>
-                      <Box className="image"><img src={item?.image} alt="" /></Box>
-                    </Grid>
-                    <Grid item xs={6} sm={6} md={8}>
-                      <Box className="contents">
-                        <Box className="name"><a href="#">{item?.name}</a></Box>
-                        <Box className="price"><img src={priceIcon} alt="" /> {item?.price}<span>{item?.mrp}</span></Box>
-                        <Box display={'flex'} width={'100%'} justifyContent={'space-between'} alignItems={'center'}>
-                          <Box className="rating"><StarIcon /> {item?.ratings}</Box>
-                          <Box className="buttons">
-                            <Button><TurnedInNotOutlinedIcon /></Button>
-                            <Button><ShoppingCartOutlinedIcon /></Button>
-                          </Box>
-                        </Box>
+            // Render either all items or first 4 items based on showAllItems state
+            recentList.slice(0, 3).map(item => (
+          <Box key={item.id} className="recently-products">
+            <Box className="product-box">
+              <Grid container spacing={2} alignItems={'center'}>
+                <Grid item xs={6} sm={6} md={4}>
+                  <Box className="image"><img src={item?.image} alt="" /></Box>
+                </Grid>
+                <Grid item xs={6} sm={6} md={8}>
+                  <Box className="contents">
+                    <Box className="name"><a href="#">{item?.name}</a></Box>
+                    <Box className="price"><img src={priceIcon} alt="" /> {item?.price}<span>{item?.mrp}</span></Box>
+                    <Box display={'flex'} width={'100%'} justifyContent={'space-between'} alignItems={'center'}>
+                      <Box className="rating"><StarIcon /> {item?.ratings}</Box>
+                      <Box className="buttons">
+                        <Button><TurnedInNotOutlinedIcon /></Button>
+                        <Button><ShoppingCartOutlinedIcon /></Button>
                       </Box>
-                    </Grid>
-                  </Grid>
-                </Box>
-              </Box>
-            ))
+                    </Box>
+                  </Box>
+                </Grid>
+              </Grid>
+            </Box>
+          </Box>
+          ))
           ) : (
-            <Box>No recently viewed items.</Box>
+          <Box>No recently viewed items.</Box>
           )}
 
         </Container>
@@ -74,4 +78,3 @@ class RecentlyViewedItems extends Component {
 }
 
 export default RecentlyViewedItems;
-
