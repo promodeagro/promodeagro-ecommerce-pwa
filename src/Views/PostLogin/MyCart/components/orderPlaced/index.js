81f19a8bca4fbb0dae9615dd4cf05c6f0b0f2a6d
furqhan bin moosa <furqhanbinmoosaptr@gmail.com>
2024-12-10 14:36:48 +0530

    cash on delivery feature add

--- src/Views/PostLogin/MyCart/components/orderPlaced/index.js
+++ src/Views/PostLogin/MyCart/components/orderPlaced/index.js
@@ -5,6 +5,10 @@ import LocalShippingIcon from "@mui/icons-material/LocalShipping";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import { Link } from "react-router-dom";
 import { useParams, useNavigate } from "react-router-dom";
+import PlaceOrderTick from "../../../../../assets/img/PlaceOrderTick.png"
+import TruckIcon from "../../../../../assets/img/TruckIcon.svg"
+import locationIcon from "../../../../../assets/img/LocationImg.svg"
+
 const OrderPlaced = (props) => {
   const [value, setValue] = useState(0);
   const { id } = useParams();
@@ -18,7 +22,10 @@ const OrderPlaced = (props) => {
   }, []);
 
   return (
-    <Container>
+
+    <>
+    
+    {/* <Container>
       <Box
         className="order-placed-container"
         data-aos="zoom-in-right"
@@ -71,7 +78,68 @@ const OrderPlaced = (props) => {
           </Box>
         </Box>
       </Box>
+    </Container> */}
+    <Container className="order_placed_container"   data-aos="zoom-in-right"
+        data-aos-offset="200"
+        data-aos-easing="ease-in-sine" sx={{minHeight:'80vh' ,display:"flex",justifyContent:"center",flexDirection:"column", alignItems:"center"}}>
+
+      <Box className="order_placed_box" sx={{display:"flex" , flexDirection:"column" , alignItems:"center"}}>
+        <img src={PlaceOrderTick} alt="" />
+<h2>Your Order Placed !</h2>
+
+<Box  className="rating_box rating d-flex align-items-center justify-content-center w-100">
+<span className="rating-info">How was everything ?</span>
+            <Rating
+              name="simple-controlled"
+              value={value}
+              onChange={(event, newValue) => {
+                setValue(newValue);
+              }}
+              size="large"
+            />
+          </Box>
+      </Box>
+
+<Box className="Delivery_details_box w-100">
+
+<Box className="select_delivery_address">
+  <span className="span_lite">
+    <img src={locationIcon} alt="" />
+    Delivery Address</span>
+  <div className="line"></div>
+  <span>Home 20-5-296 Qazipura, Shah Ali banda, Hyderabad 500065</span>
+</Box>
+
+<Box className="select_delivery_address">
+<span className="span_lite">
+  <img src={TruckIcon} alt="" />
+  Shipment Time Slot</span>
+<div className="line"></div>
+<span>3 Item</span>
+<div className="line"></div>
+<span>05 Nov, Tuesday 06:00 AM - 07:00 AM </span>
+<span>Rs 463</span>
+</Box>
+
+
+<Box className="payment_details_box">
+<Box className="payment_details">
+  <div>Payment status <span className="cod">Cash on Delivery</span></div>
+  <div>Order amount <span className="amount">Rs. 463</span></div>
+</Box>
+</Box>
+</Box>
+
+
+<Box sx={{marginTop:"20px" , marginBottom:"20px"}}>
+  <Button  onClick={()=> navigate("/")} className="common-btn">
+  Continue Shopping
+  </Button>
+</Box>
+
+
     </Container>
+    </>
   );
 };
 
