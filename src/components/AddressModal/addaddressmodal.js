f145be239d49ec720560fb4ebfd236ec6c37375e
shaistha-samreen <shaisthasamreen786@gmail.com>
2024-12-15 13:22:52 +0530

    worked on the edit address modal

--- src/components/AddressModal/addaddressmodal.js
+++ src/components/AddressModal/addaddressmodal.js
@@ -6,7 +6,7 @@ import { ValidationEngine } from "Views/Utills/helperFunctions";
 import Homeicon from "../../assets/img/homeicon.png";
 import Workicon from "../../assets/img/workicon.png";
 import Othericon from "../../assets/img/othericon.png";
-import { postAddress } from "../../Redux/Address/AddressThunk";
+import { updateAddress, postAddress } from "../../Redux/Address/AddressThunk";
 import { loginDetails } from "Views/Utills/helperFunctions";
 
 const addressValidationSchema = {
@@ -59,11 +59,31 @@ class AddAddressModal extends Component {
       area: "",
       pincode: "",
       isSubmitting: false,
-      selectedAddressType: "", 
+      selectedAddressType: "",
+      addressId: "",
+      initialLoad: true, // Ensures props are loaded only once
     };
   }
 
-  
+  static getDerivedStateFromProps(nextProps, prevState) {
+    if (
+      nextProps.addressData &&
+      prevState.initialLoad // Ensures this runs only on initial load
+    ) {
+      return {
+        fullName: nextProps.addressData.name || "",
+        flatNumber: nextProps.addressData.house_number || "",
+        landmark: nextProps.addressData.landmark_area || "",
+        phone: nextProps.addressData.phoneNumber || "",
+        area: nextProps.addressData.address || "",
+        pincode: nextProps.addressData.zipCode || "",
+        selectedAddressType: nextProps.addressData.address_type || "",
+        addressId: nextProps.addressData.addressId || "",
+        initialLoad: false, // Disable further updates from props
+      };
+    }
+    return null;
+  }
 
   validateForm = () => {
     const { fullName, flatNumber, landmark, phone, area, pincode } = this.state;
@@ -81,53 +101,64 @@ class AddAddressModal extends Component {
   handleAddressSubmit = async (e) => {
     e.preventDefault();
     const errorData = this.validateForm();
-    
+
     if (errorData.isValid) {
-      const { fullName, flatNumber, landmark, phone, area, pincode, selectedAddressType } = this.state;
-  
-      // Get userId from loginDetails
-      const loginData = loginDetails(); // Fetch login data from localStorage
-      const userId = loginData ? loginData.userId : null; // Get userId from login details
-  
-      // Check if userId exists
+      const {
+        fullName,
+        flatNumber,
+        landmark,
+        phone,
+        area,
+        pincode,
+        selectedAddressType,
+        addressId,
+      } = this.state;
+
+      const loginData = loginDetails();
+      const userId = loginData ? loginData.userId : null;
+
       if (!userId) {
-        // Handle missing userId error (e.g., show a message or handle accordingly)
         console.error("User ID is missing.");
         return;
       }
-  
-      // Mapping the request body as per the required key names
-      const requestBody = {
-        userId: userId, // User ID from login details
-        address: {
-          name: fullName,                // Full name
-          address: area,                 // Area/Locality
-          phoneNumber: phone,            // Phone number
-          landmark_area: landmark,       // Landmark
-          zipCode: pincode,              // Pincode
-          house_number: flatNumber,      // House number (Flat No./House No./Building No.)
-          address_type: selectedAddressType, // Add address type (Home, Work, Other)
 
+      const requestBody = {
+        userId: userId,
+        address: {
+          name: fullName,
+          address: area,
+          phoneNumber: phone,
+          landmark_area: landmark,
+          zipCode: pincode,
+          house_number: flatNumber,
+          address_type: selectedAddressType,
         },
       };
-  
+
       this.setState({ isSubmitting: true });
-  
+
       try {
-        // Assuming `this.props.postAddress` is the async thunk to make the API call
-        await this.props.postAddress(requestBody);
+        if (addressId) {
+          // Update existing address
+          requestBody.addressId = addressId;
+          await this.props.updateAddress(requestBody);
+        } else {
+          // Add new address
+          await this.props.postAddress(requestBody);
+        }
         this.setState({ isSubmitting: false });
-        this.props.handleClose(); // Close modal on success
+        this.props.handleClose();
       } catch (error) {
         this.setState({ isSubmitting: false });
         console.error("Error submitting address:", error);
       }
     }
   };
-    
+
   handleAddressTypeSelection = (type) => {
     this.setState({ selectedAddressType: type });
   };
+
   render() {
     const { open, handleClose } = this.props;
     const {
@@ -138,8 +169,8 @@ class AddAddressModal extends Component {
       area,
       pincode,
       isSubmitting,
+      selectedAddressType,
     } = this.state;
-    const { selectedAddressType } = this.state;
 
     return (
       <Modal open={open} onClose={handleClose}>
@@ -147,45 +178,34 @@ class AddAddressModal extends Component {
           <span className="address-text">Add Address</span>
           <p className="saveaddressas">Save Address As</p>
           <div className="iconcontainer">
-            <a className="icons-wrapper">
-              <img src={Homeicon} alt="Home" className="icon" />
+            {["Home", "Work", "Other"].map((type) => (
               <a
-                className="icontext"
-                onClick={() => this.handleAddressTypeSelection("Home")}
-                style={{
-                  color: selectedAddressType === "Home" ? "#1F9151" : "gray",
-                  fontWeight: selectedAddressType === "Home" ? "600" : "400",
-                }}
+                key={type}
+                className="icons-wrapper"
+                onClick={() => this.handleAddressTypeSelection(type)}
               >
-                Home
+                <img
+                  src={
+                    type === "Home"
+                      ? Homeicon
+                      : type === "Work"
+                      ? Workicon
+                      : Othericon
+                  }
+                  alt={type}
+                  className="icon"
+                />
+                <span
+                  className="icontext"
+                  style={{
+                    color: selectedAddressType === type ? "#1F9151" : "gray",
+                    fontWeight: selectedAddressType === type ? "600" : "400",
+                  }}
+                >
+                  {type}
+                </span>
               </a>
-            </a>
-            <a className="icons-wrapper">
-              <img src={Workicon} alt="Work" className="icon" />
-              <a
-                className="icontext"
-                onClick={() => this.handleAddressTypeSelection("Work")}
-                style={{
-                  color: selectedAddressType === "Work" ? "#1F9151" : "gray",
-                  fontWeight: selectedAddressType === "Work" ? "600" : "400",
-                }}
-              >
-                Work
-              </a>
-            </a>
-            <a className="icons-wrapper">
-              <img src={Othericon} alt="Other" className="icon" />
-              <a
-                className="icontext"
-                onClick={() => this.handleAddressTypeSelection("Other")}
-                style={{
-                  color: selectedAddressType === "Other" ? "#1F9151" : "gray",
-                  fontWeight: selectedAddressType === "Other" ? "600" : "400",
-                }}
-              >
-                Other
-              </a>
-            </a>
+            ))}
           </div>
 
           <Grid container spacing={2}>
@@ -194,33 +214,35 @@ class AddAddressModal extends Component {
                 <span className="para">
                   Full Name<p className="para1">*</p>
                 </span>
+
                 <TextField
                   fullWidth
                   value={fullName}
                   onChange={(e) => this.setState({ fullName: e.target.value })}
-                  variant="outlined"
                 />
               </Box>
+
               <Box className="labelform">
                 <span className="para">
                   Flat No./ House No./ Building No.<p className="para1">*</p>
                 </span>
+
                 <TextField
                   fullWidth
                   value={flatNumber}
                   onChange={(e) =>
                     this.setState({ flatNumber: e.target.value })
                   }
-                  variant="outlined"
                 />
               </Box>
+
               <Box className="labelform">
                 <span className="para">Landmark</span>
+
                 <TextField
                   fullWidth
                   value={landmark}
                   onChange={(e) => this.setState({ landmark: e.target.value })}
-                  variant="outlined"
                 />
               </Box>
             </Grid>
@@ -229,43 +251,38 @@ class AddAddressModal extends Component {
                 <span className="para">
                   Phone No.<p className="para1">*</p>
                 </span>
+
                 <TextField
                   fullWidth
                   value={phone}
                   onChange={(e) => this.setState({ phone: e.target.value })}
-                  variant="outlined"
                 />
               </Box>
+
               <Box className="labelform">
                 <span className="para">
                   Area/Locality<p className="para1">*</p>
                 </span>
+
                 <TextField
                   fullWidth
                   value={area}
                   onChange={(e) => this.setState({ area: e.target.value })}
-                  variant="outlined"
                 />
               </Box>
               <Box className="labelform">
                 <span className="para">
                   Pincode<p className="para1">*</p>
                 </span>
+
                 <TextField
                   fullWidth
                   value={pincode}
                   onChange={(e) => this.setState({ pincode: e.target.value })}
-                  variant="outlined"
                 />
               </Box>
             </Grid>
           </Grid>
-          <Box className="checkbox">
-            <span>
-              <Checkbox color="success" />
-            </span>
-            <span>Set this Address as default</span>
-          </Box>
           <Box sx={{ marginTop: "16px" }}>
             {isSubmitting ? (
               <CircularProgress color="success" />
@@ -287,6 +304,7 @@ class AddAddressModal extends Component {
 }
 
 const mapDispatchToProps = (dispatch) => ({
+  updateAddress: (data) => dispatch(updateAddress(data)),
   postAddress: (data) => dispatch(postAddress(data)),
 });
 
