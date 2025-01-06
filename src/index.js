9c0200abddaa54385c0bc1eeee7bd29c0a0502a4
furqhan bin moosa <furqhanbinmoosaptr@gmail.com>
2025-01-06 16:27:48 +0530

    removed report vitual

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import "./assets/sass/style.scss";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+// import reportWebVitals from "./reportWebVitals";
 import AOS from "aos";
-import "aos/dist/aos.css";
+// import "aos/dist/aos.css";
 // import * as serviceWorkerRegistration from './serviceWorkerRegistration'; // Import service worker registration
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -18,7 +18,7 @@ root.render(
   </Provider>
 );
 
-AOS.init();
+// AOS.init();
 
 // Register service worker to make it a PWA
 // serviceWorkerRegistration.register();
@@ -26,4 +26,4 @@ AOS.init();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
