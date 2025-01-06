276aef4ba03c39fd7388c05d8526c3ee58cc16b8
furqhan bin moosa <furqhanbinmoosaptr@gmail.com>
2025-01-06 16:34:20 +0530

    n/a

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import "./assets/sass/style.scss";
 import App from "./App";
-// import reportWebVitals from "./reportWebVitals";
+import reportWebVitals from "./reportWebVitals";
 import AOS from "aos";
-// import "aos/dist/aos.css";
-// import * as serviceWorkerRegistration from './serviceWorkerRegistration'; // Import service worker registration
+import "aos/dist/aos.css";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'; // Import service worker registration
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -18,12 +18,12 @@ root.render(
   </Provider>
 );
 
-// AOS.init();
+AOS.init();
 
 // Register service worker to make it a PWA
-// serviceWorkerRegistration.register();
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+reportWebVitals();
