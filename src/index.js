d8de70cf1eb7607c19e24a094945bf01b7120c6b
shaistha-samreen <shaisthasamreen786@gmail.com>
2025-01-07 13:23:28 +0530

    removed the service worker

--- src/index.js
+++ src/index.js
@@ -8,20 +8,15 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import * as serviceWorkerRegistration from './serviceWorkerRegistration'; // Import service worker registration
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 
 root.render(
   <Provider store={store}>
     <App />
   </Provider>
 );
-
 AOS.init();
-
-// Register service worker to make it a PWA
-serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
