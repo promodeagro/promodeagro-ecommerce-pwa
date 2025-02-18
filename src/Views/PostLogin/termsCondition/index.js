30949b0a7208477be71791aa63de23e2a89f7510
Fatima Tabassum <fatimatabassum743@gmail.comm>
2025-02-18 21:05:06 +0530

    added links

--- src/Views/PostLogin/termsCondition/index.js
+++ src/Views/PostLogin/termsCondition/index.js
@@ -16,21 +16,21 @@ class TermsCondition extends Component {
             <h1>Terms & Condition</h1>
             <p>
               Thank you for accessing/shopping at{" "}
-              <a href="www.promodeagro.com">www.promodeagro.com</a>. This site
+              <a href="https://promodeagro.com/">promodeagro.com</a>. This site
               is owned by Promode Agro Farms (hereinafter referred to as{" "}
-              <a href="www.promodeagro.com">www.promodeagro.com</a>). By
+              <a href="https://promodeagro.com/">promodeagro.com</a>). By
               accessing, shopping on this site, you indicate your unconditional
               acceptance of these terms & conditions. We reserve this right, in
               our sole discretion, to update or revise these terms & conditions.
               Continued use of the site following the posting of any changes to
               the ‘terms & conditions’, constitutes your acceptance of those
-              changes. At <a href="www.promodeagro.com">www.promodeagro.com</a>{" "}
+              changes. At <a href="https://promodeagro.com/">promodeagro.com</a>{" "}
               we try our best to create a space where you can explore and shop
               for all your favorite things in a safe and secure environment. All
               products and information displayed on{" "}
-              <a href="www.promodeagro.com">www.promodeagro.com</a> constitutes
+              <a href="https://promodeagro.com/">promodeagro.com</a> constitutes
               an "invitation to offer".{" "}
-              <a href="www.promodeagro.com">www.promodeagro.com</a> reserves the
+              <a href="https://promodeagro.com/">promodeagro.com</a> reserves the
               right to accept or reject your offer. Your order for purchase,
               constitutes your "offer" which shall be subject to the terms and
               conditions as listed below.
@@ -41,7 +41,7 @@ class TermsCondition extends Component {
               into contracts under applicable laws. Persons who are "incompetent
               to contract", within the meaning of the Indian Contract Act, 1872
               including un-discharged insolvents etc. are not eligible to use
-              the Site. <a href="www.promodeagro.com">www.promodeagro.com</a>{" "}
+              the Site. <a href="https://promodeagro.com/">promodeagro.com</a>{" "}
               reserves the right to terminate your access to the Site if it
               discovers that you are under the age of 18 years or suffers from
               any other disability, as recognized under Indian Contract Act,
@@ -50,12 +50,12 @@ class TermsCondition extends Component {
             <strong>2. Membership</strong>
             <p>
               Although it's not essential to have an account to shop with{" "}
-              <a href="www.promodeagro.com">www.promodeagro.com</a>, you can
+              <a href="https://promodeagro.com/">promodeagro.com</a>, you can
               shop as a guest. As a member, you agree to provide true, accurate,
               current, and complete information about yourself as prompted by
               the site's registration form. Registration where prohibited under
               any law shall be void.{" "}
-              <a href="www.promodeagro.com">www.promodeagro.com</a> reserves the
+              <a href="https://promodeagro.com/">promodeagro.com</a> reserves the
               right to revoke or terminate your registration for any reason at
               any time, without notice.
             </p>
@@ -71,23 +71,23 @@ class TermsCondition extends Component {
             <p>
               All reviews, comments, feedback, postcards, suggestions, ideas,
               and other submissions disclosed, submitted or offered to{" "}
-              <a href="www.promodeagro.com">www.promodeagro.com</a> directly or
+              <a href="https://promodeagro.com/">promodeagro.com</a> directly or
               otherwise disclosed, submitted or offered in connection with your
               use of this Site (collectively referred to "Comments") will remain{" "}
-              <a href="www.promodeagro.com">www.promodeagro.com</a> property.
+              <a href="https://promodeagro.com/">promodeagro.com</a> property.
               Such disclosure, submission or offer of any comments shall
               constitute an assignment to{" "}
-              <a href="www.promodeagro.com">www.promodeagro.com</a> of all
+              <a href="https://promodeagro.com/">promodeagro.com</a> of all
               worldwide rights, titles and interests in all copyrights and other
               intellectual properties in the comments,thus, it exclusively owns
               all such rights, titles and interests and shall not be limited in
               any way in its use, commercial or otherwise.{" "}
-              <a href="www.promodeagro.com">www.promodeagro.com</a> will be
+              <a href="https://promodeagro.com/">promodeagro.com</a> will be
               entitled to use, reproduce, disclose, modify, adapt, create
               derivative works from, publish, display and distribute any
               comments you submit for any purpose whatsoever, without
               restriction and without compensating you in any way.{" "}
-              <a href="www.promodeagro.com">www.promodeagro.com</a> is and shall
+              <a href="https://promodeagro.com/">promodeagro.com</a> is and shall
               be under no obligation
             </p>
             <p>
@@ -104,17 +104,17 @@ class TermsCondition extends Component {
               of "spam".
             </p>
             <p>
-              <a href="www.promodeagro.com">www.promodeagro.com</a> does not
+              <a href="https://promodeagro.com/">promodeagro.com</a> does not
               regularly review posted comments, but does reserve the right (but
               not the obligation) to monitor and edit or remove any comment
               submitted to the Site. You grant{" "}
-              <a href="www.promodeagro.com">www.promodeagro.com</a> the right to
+              <a href="https://promodeagro.com/">promodeagro.com</a> the right to
               use the name that you submit in connection with any of the posted
               comments. You agree not to use a false email address, impersonate
               any person or entity, or otherwise mislead as to the origin of any
               Comments you submit. You are and shall remain solely responsible
               for the content of any comments you make and you agree to
-              indemnify <a href="www.promodeagro.com">www.promodeagro.com</a>{" "}
+              indemnify <a href="https://promodeagro.com/">promodeagro.com</a>{" "}
               and its affiliates for all claims resulting from any Comments you
               submit, we take no responsibility and assume no liability for any
               comments submitted by you or any third party.
@@ -123,12 +123,12 @@ class TermsCondition extends Component {
               5. Accuracy of Content/ Information of Products on the Web Site
             </strong>
             <p>
-              While <a href="www.promodeagro.com">www.promodeagro.com</a>{" "}
+              While <a href="https://promodeagro.com/">promodeagro.com</a>{" "}
               strives to provide accurate product and pricing information,
               typographical errors may occur. In the event that a product is
               listed at an incorrect price or with incorrect information due to
               an error in pricing or product information,{" "}
-              <a href="www.promodeagro.com">www.promodeagro.com</a> shall have
+              <a href="https://promodeagro.com/">promodeagro.com</a> shall have
               the right, on our sole discretion, to modify the price of the
               products, or information of the products or to refuse or cancel
               any orders placed for that product, unless the product has already
