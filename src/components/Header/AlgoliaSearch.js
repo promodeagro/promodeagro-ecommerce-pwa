1a8aca1b74a63806115d1d98607c856ab9158b5c
Fatima Tabassum <fatimatabassum743@gmail.comm>
2025-02-28 16:09:36 +0530

    resolve issue

--- src/components/Header/AlgoliaSearch.js
+++ src/components/Header/AlgoliaSearch.js
@@ -1,9 +1,21 @@
-import React, { Component } from "react";
-import { InstantSearch, Configure, connectSearchBox, connectHits, connectStateResults } from "react-instantsearch-dom";
-import { Box, TextField, InputAdornment, CircularProgress } from "@mui/material";
+import React, { Component, useState, useEffect } from "react";
+import {
+  InstantSearch,
+  Configure,
+  connectSearchBox,
+  connectHits,
+  connectStateResults,
+} from "react-instantsearch-dom";
+import {
+  Box,
+  TextField,
+  InputAdornment,
+  CircularProgress,
+} from "@mui/material";
 import algoliasearch from "algoliasearch";
 import SearchProductItemView from "../AddRemoveProductComponents/searchProductView";
 import searchIcon from "../../assets/img/search-icon.png";
+import { debounce } from "lodash";
 
 class AlgoliaSearch extends Component {
   constructor(props) {
@@ -12,7 +24,6 @@ class AlgoliaSearch extends Component {
       searchClient: null, // Store Algolia search client
       loading: true, // Loading state for API keys
       placeholderIndex: 0,
-      searchBoxKey: 0, // To force re-render of search box
     };
     this.searchBoxRef = React.createRef();
 
@@ -27,7 +38,8 @@ class AlgoliaSearch extends Component {
     this.fetchAlgoliaKeys();
     this.intervalId = setInterval(() => {
       this.setState((prevState) => ({
-        placeholderIndex: (prevState.placeholderIndex + 1) % this.placeholderTexts.length,
+        placeholderIndex:
+          (prevState.placeholderIndex + 1) % this.placeholderTexts.length,
       }));
     }, 2000);
   }
@@ -38,13 +50,17 @@ class AlgoliaSearch extends Component {
 
   async fetchAlgoliaKeys() {
     try {
-      const response = await fetch("https://ijy06nbhob.execute-api.ap-south-1.amazonaws.com/getSecrets");
+      const response = await fetch(
+        "https://ijy06nbhob.execute-api.ap-south-1.amazonaws.com/getSecrets"
+      );
       const data = await response.json();
-   
-      
+
       if (data.ALGOLIA_APP_ID && data.ALGOLIA_API_KEY) {
         this.setState({
-          searchClient: algoliasearch(data.ALGOLIA_APP_ID, data.ALGOLIA_API_KEY),
+          searchClient: algoliasearch(
+            data.ALGOLIA_APP_ID,
+            data.ALGOLIA_API_KEY
+          ),
           loading: false,
         });
       } else {
@@ -57,16 +73,10 @@ class AlgoliaSearch extends Component {
     }
   }
 
-
-  
-
   render() {
-    const { showResult, onFocus, matches, inputRef,searchBgClick } = this.props;
-    const { placeholderIndex, searchBoxKey, searchClient, loading } = this.state;
-
-    if (loading) {
-      return <CircularProgress />;
-    }
+    const { showResult, onFocus, matches, inputRef, searchBgClick } =
+      this.props;
+    const { placeholderIndex, searchClient } = this.state;
 
     if (!searchClient) {
       return <p>Error initializing search. Please try again later.</p>;
@@ -74,7 +84,7 @@ class AlgoliaSearch extends Component {
 
     return (
       <>
-        <InstantSearch searchClient={searchClient} indexName="products" key={searchBoxKey}>
+        <InstantSearch searchClient={searchClient} indexName="products">
           <Box className="search-container">
             <CustomSearchBox
               onFocus={onFocus}
@@ -86,59 +96,81 @@ class AlgoliaSearch extends Component {
             />
             <SearchResults>
               {({ hasResults, hasQuery }) => (
-                <Box className={`search-results ${showResult && hasQuery ? "active" : ""}`} style={matches ? { width: "100vw" } : {}}>
+                <Box
+                  className={`search-results ${
+                    showResult && hasQuery ? "active" : ""
+                  }`}
+                  style={matches ? { width: "100vw", marginTop: "20px" } : {}}
+                >
                   {showResult && hasResults && <CustomHits />}
                 </Box>
               )}
             </SearchResults>
           </Box>
-          <Configure hitsPerPage={5} />
+          <Configure hitsPerPage={10} />
         </InstantSearch>
-        <Box className='search-results-bg' onClick={searchBgClick} />
+        <Box className="search-results-bg" onClick={searchBgClick} />
       </>
     );
   }
 }
 
 // Search Results Component
-const SearchResults = connectStateResults(({ searchState, searchResults, children }) => {
-  const hasResults = searchResults?.nbHits !== 0;
-  const hasQuery = searchState?.query?.length > 0;
-  return children({ hasResults, hasQuery, searchResults });
-});
+const SearchResults = connectStateResults(
+  ({ searchState, searchResults, children }) => {
+    const hasResults = searchResults?.nbHits !== 0;
+    const hasQuery = searchState?.query?.length > 0;
+    return children({ hasResults, hasQuery, searchResults });
+  }
+);
 
-// Custom Search Box
-const CustomSearchBox = connectSearchBox(({ currentRefinement, refine, onFocus, placeholder, inputRef }) => {
-  return (
-    <TextField
-      id="outlined-search"
-      className="search"
-      variant="outlined"
-      value={currentRefinement}
-      onChange={(e) => refine(e.target.value)}
-      onFocus={onFocus}
-      inputRef={(ref) => {
-        if (ref) {
-          inputRef(ref);
-          ref.refine = refine; // Attach refine function
-        }
-      }}
-      InputProps={{
-        startAdornment: (
-          <InputAdornment position="start">
-            <img src={searchIcon} alt="Search" />
-          </InputAdornment>
-        ),
-      }}
-      placeholder={placeholder}
-    />
-  );
-});
+// ✅ Properly Debounced Custom Search Box
+const CustomSearchBox = connectSearchBox(
+  ({ currentRefinement, refine, onFocus, placeholder, inputRef }) => {
+    const [searchTerm, setSearchTerm] = useState(currentRefinement || "");
+    console.log(searchTerm,"search");
+    const debouncedRefine = debounce(refine, 300); // 300ms debounce
 
+
+    useEffect(() => {
+      debouncedRefine(searchTerm);
+      return () => debouncedRefine.cancel(); // Cleanup on unmount
+    }, [searchTerm]);
+
+    return (
+      <TextField
+        id="outlined-search"
+        className="search"
+        variant="outlined"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        onFocus={onFocus}
+        inputRef={(ref) => {
+          if (ref) {
+            inputRef(ref);
+            ref.refine = refine;
+          }
+        }}
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <img src={searchIcon} alt="Search" />
+            </InputAdornment>
+          ),
+        }}
+        placeholder={placeholder}
+      />
+    );
+  }
+);
 
 // Search Hits Component
-const CustomHits = connectHits(({ hits }) => (
-  hits.length === 0 ? <p className="no-data">No data found</p> : <SearchProductItemView productList={hits} />
-));
+const CustomHits = connectHits(({ hits }) =>
+  hits.length === 0 ? (
+    <p className="no-data">No data found</p>
+  ) : (
+    <SearchProductItemView productList={hits} />
+  )
+);
 
 export default AlgoliaSearch;
