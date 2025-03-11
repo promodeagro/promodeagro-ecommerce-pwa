82df199a686cf8715afb0ad1518261e36ac6ea03
Fatima Tabassum <fatimatabassum743@gmail.comm>
2025-03-11 19:46:04 +0530

    resolve issue

--- src/components/Header/searchResults.js
+++ src/components/Header/searchResults.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { Box } from "@mui/material";
 import AlgoliaSearch from "./AlgoliaSearch";
 import { withRouter } from "components/withRouter";
 
@@ -7,28 +6,10 @@ class SearchResults extends Component {
   constructor(props) {
     super(props);
     this.searchInputRef = React.createRef();
-    this.state = {
-      matches: window.matchMedia("(max-width: 600px)").matches,
-    };
-  }
-
-  componentDidMount() {
-    window
-      .matchMedia("(max-width: 600px)")
-      .addEventListener("change", (e) => this.setState({ matches: e.matches }));
   }
 
   render() {
-    const { matches } = this.state;
-
-    return (
-      <Box
-        className="search-wrapper"
-        style={matches ? { width: "100vw" } : { width: "650px" }}
-      >
-        <AlgoliaSearch inputRef={this.searchInputRef} />
-      </Box>
-    );
+    return <AlgoliaSearch inputRef={this.searchInputRef} />;
   }
 }
 
